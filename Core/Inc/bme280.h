241d4ee3089a50ba5cb4dd125fb76e9c622626ab
anshulkrishnakumar <anshulkrishnakumar@gmail.com>
2026-09-22 23:31:54 -0400

    configured and verified BME280 registers

--- Core/Inc/bme280.h
+++ Core/Inc/bme280.h
@@ -5,6 +5,11 @@
 
 #define BME280_ADDR 0x76
 
+#define BME280_REG_CTRL_HUM     0xF2 // humidity oversampling configurations
+#define BME280_REG_CTRL_MEAS    0xF4 // temperature + presssure oversampling configurations
+#define BME280_REG_CONFIG       0xF5 // data output rate
+#define BME280_REG_DATA         0xF7 // storing data
+
 HAL_StatusTypeDef BME280_Init(I2C_HandleTypeDef *hi2sc);
 
 #endif
--- Core/Src/bme280.c
+++ Core/Src/bme280.c
@@ -1,9 +1,19 @@
 #include "bme280.h"
+#include "usart.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 
 HAL_StatusTypeDef BME280_Init(I2C_HandleTypeDef *hi2c) {
     uint8_t chip_id = 0;
 
-    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c, BME280_ADDR << 1, 0xD0, I2C_MEMADD_SIZE_8BIT, &chip_id, 1, HAL_MAX_DELAY);
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c,
+                                                BME280_ADDR << 1,
+                                                0xD0,
+                                                I2C_MEMADD_SIZE_8BIT,
+                                                &chip_id,
+                                                1,
+                                                HAL_MAX_DELAY);
     
     if (status != HAL_OK) {
         return status;
@@ -13,6 +23,56 @@ HAL_StatusTypeDef BME280_Init(I2C_HandleTypeDef *hi2c) {
         return HAL_ERROR;
     }
 
-    return HAL_OK;
+    uint8_t ctrl_hum = 0x01; // humidity oversampling x1
+    uint8_t ctrl_meas = 0x27; // temperature x1, pressure x1, sensor normal mode
 
+    HAL_I2C_Mem_Write(hi2c,
+                      BME280_ADDR << 1,
+                      BME280_REG_CTRL_HUM,
+                      I2C_MEMADD_SIZE_8BIT,
+                      &ctrl_hum,
+                      1,
+                      HAL_MAX_DELAY);
+                      
+    HAL_I2C_Mem_Write(hi2c,
+                      BME280_ADDR << 1,
+                      BME280_REG_CTRL_MEAS,
+                      I2C_MEMADD_SIZE_8BIT,
+                      &ctrl_meas,
+                      1,
+                      HAL_MAX_DELAY);    
+
+    uint8_t reg_value;
+    char msg[50];
+
+    HAL_I2C_Mem_Read(hi2c,
+                    BME280_ADDR << 1,
+                    BME280_REG_CTRL_HUM,
+                    I2C_MEMADD_SIZE_8BIT,
+                    &reg_value,
+                    1,
+                    HAL_MAX_DELAY);
+
+    snprintf(msg, sizeof(msg), "CTRL_HUM: 0x%02X\r\n", reg_value);
+    HAL_UART_Transmit(&huart2,
+                    (uint8_t *)msg,
+                    strlen(msg),
+                    HAL_MAX_DELAY);
+
+    HAL_I2C_Mem_Read(hi2c,
+                    BME280_ADDR << 1,
+                    BME280_REG_CTRL_MEAS,
+                    I2C_MEMADD_SIZE_8BIT,
+                    &reg_value,
+                    1,
+                    HAL_MAX_DELAY);
+
+    snprintf(msg, sizeof(msg), "CTRL_MEAS: 0x%02X\r\n", reg_value);
+    HAL_UART_Transmit(&huart2,
+                    (uint8_t *)msg,
+                    strlen(msg),
+                    HAL_MAX_DELAY);
+    
+    
+    return HAL_OK;
 }
